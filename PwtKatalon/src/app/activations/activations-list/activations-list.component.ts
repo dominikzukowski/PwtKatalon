e9189b1d5f792f59501a3a58274690bc2538e64d
Dominik Zukowski <dzukowski@moneygram.com>
2018-10-03 11:25:26 +0200

    CHANGE table style and the way to open details

--- PwtKatalon/src/app/activations/activations-list/activations-list.component.ts
+++ PwtKatalon/src/app/activations/activations-list/activations-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IActivation } from '../activation';
 import { ActivationService } from '../activation.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './activations-list.component.html',
@@ -10,16 +11,20 @@ export class ActivationsListComponent implements OnInit {
   activations: IActivation[];  
   errorMessage: any;
   
-  constructor(private service: ActivationService) {
+  constructor(private service: ActivationService, private router: Router) {
     this.activations = [];
+  }
+
+  ngOnInit() {
+    this.populateActivations();
   }
 
   populateActivations() {
     this.service.getActivations().subscribe((res) => this.activations = res,
     error => this.errorMessage = <any>error );
   }
-
-  ngOnInit() {
-    this.populateActivations();
+  
+  openActivationDetails(id: number) {
+    this.router.navigate(['/activation', id]);
   }
 }
--- PwtKatalon/src/app/scheduler/scheduler-list/scheduler-list.component.ts
+++ PwtKatalon/src/app/scheduler/scheduler-list/scheduler-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ISchedule } from '../schedule';
 import { ScheduleService } from '../schedule.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './scheduler-list.component.html',
@@ -10,17 +11,21 @@ export class SchedulerListComponent implements OnInit {
   schedules: ISchedule[];
   errorMessage: any;
 
-  constructor(private service: ScheduleService) {
+  constructor(private service: ScheduleService, private router: Router) {
     this.schedules = [];
-   }
-
-  populateSchedules(){
-    this.service.getSchedules().subscribe((res)=>this.schedules = res,
-    error => this.errorMessage = <any>error);
   }
 
   ngOnInit() {
     this.populateSchedules();
+  }
+
+  populateSchedules() {
+    this.service.getSchedules().subscribe((res) => this.schedules = res,
+      error => this.errorMessage = <any>error);
+  }
+
+  openSchedulerDetails(id: number) {
+    this.router.navigate(['/scheduler', id]);
   }
 
 }
