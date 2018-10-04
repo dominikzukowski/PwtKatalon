b5cbdd465abb607685b3313f70471cf10b7e88cb
Dominik Zukowski <dzukowski@moneygram.com>
2018-10-04 13:49:11 +0200

    ADD Pagination

--- PwtKatalonApi/Startup.cs
+++ PwtKatalonApi/Startup.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,8 +28,16 @@ namespace PwtKatalonApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+            services.AddScoped<IUrlHelper>(factory =>
+                {
+                    var actionContext = factory.GetService<IActionContextAccessor>()
+                                               .ActionContext;
+                    return new UrlHelper(actionContext);
+                }
+                );
             services.AddMvc();
-            
+
             var connection = @"Server=QMNACMS8266\SQL2014;Database=pwtkatalon;User ID=katalon_user;Password=katalon_pass;ConnectRetryCount=0";
             services.AddDbContext<pwtkatalonContext>(options => options.UseSqlServer(connection));
         }
---
+++ PwtKatalonApi/Utils/Paging/PagingOutputModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PwtKatalonApi.Utils.Paging
+{
+    public class PagingOutputModel<T>
+    {
+        public PagingHeader Paging { get; set; }
+        public List<LinkInfo> Links { get; set; }
+        public List<T> Items { get; set; }
+    }
+}
