c79df8936398d3cbcea217a981961f4743ae1730
Dominik Zukowski <dominik.zukowski@onwelo.com>
2018-10-08 08:50:11 +0200

    upchartcolors

---
+++ PwtKatalon/src/app/chartcolors.ts
@@ -0,0 +1,8 @@
+export const chartColors = {
+    errorColor: "rgba(0, 0, 255, 1)",
+    errorColorTransparent: "rgba(0, 0, 255, 0.3)",
+    passedColor: "rgba(0, 150, 0, 1)",
+    passedColorTransparent: "rgba(0, 150, 0, 0.3)",
+    failedColor: "rgba(255, 0, 0, 1)",
+    failedColorTransaprent: "rgba(255, 0, 0, 0.3)"
+};
