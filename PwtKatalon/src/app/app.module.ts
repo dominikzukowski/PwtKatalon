6388c1361b2f29bce6d477dbba4e00e05a2ce7ff
Dominik Zukowski <dominik.zukowski@onwelo.com>
2018-10-14 22:26:57 +0200

    ADD Chart module

--- PwtKatalon/src/app/app.module.ts
+++ PwtKatalon/src/app/app.module.ts
@@ -2,31 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { ChartsModule } from 'ng2-charts';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ChartComponent } from './chart/chart/chart.component';
 import { ActivationsModule } from './activations/activations.module';
+import { ChartModule } from './chart/chart.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChartComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
-    ReactiveFormsModule,
-    ChartsModule,
     NgbModule.forRoot(),
     RouterModule.forRoot([
-      {path: 'charts', component:ChartComponent},
       {path: '', redirectTo: 'activations', pathMatch: 'full'},
       {path: '**', redirectTo: 'activations', pathMatch: 'full'}
     ], {onSameUrlNavigation: 'reload'}),
     ActivationsModule,
+    ChartModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
